41343eab3fdcb7da06d2155d23c7570123ad1fee
rogerin07 <rogersantosbezerra@gmail.com>
2025-09-22 23:26:36 -0300

    hoje é dia 22/09

--- frontend/src/pages/Messages.jsx
+++ frontend/src/pages/Messages.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Messages = () => {
-  return <div>Messages</div>;
+  return <div className="text-3xl hover:text-blue-300">Messages</div>;
 };
 
 export default Messages;
