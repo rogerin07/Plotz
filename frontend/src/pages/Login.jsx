e3cb719a20ea7aadfe8ea9feb0aa1cd7c5a7876b
rogerin07 <rogersantosbezerra@gmail.com>
2025-09-22 23:23:09 -0300

    teste

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 
 const Login = () => {
-  return <div className="min-h-screen flex flex-col md:flex-row"> teste
-  </div>;
+  return (
+    <div className="min-h-screen flex flex-col md:flex-row">
+      isso é um teste
+    </div>
+  );
 };
 
 export default Login;
